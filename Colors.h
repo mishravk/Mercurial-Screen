957ab9084ccfad32b9ec8b83ecfa72db4b4652dd
mishravk <microvikku@gmail.com>
2020-01-16 10:34:08 +0530

    Update Colors.h

--- Colors.h
+++ Colors.h
@@ -1,25 +1,3 @@
-/*
- * Copyright (c) 2017 Evan Kale
- * Email: EvanKale91@gmail.com
- * Web: www.youtube.com/EvanKale
- * Social: @EvanKale91
- *
- * This file is part of ArduinoGlowingLEDs.
- *
- * ArduinoGlowingLEDs is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 const PROGMEM  uint8_t colors[]  = {
   0x12, 0x61, 0x6b,
   0xf, 0x67, 0x63,
